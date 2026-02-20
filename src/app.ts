ceb52a28791c82f6bce11d1709affc2da9452d1b
DaliaLopez <1112046308@u.icesi.edu.co>
2026-02-20 08:33:50 -0500

    Second commit

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,13 @@
 import express, {Router} from 'express';
 import { NODE_ENV, PORT } from './config';
 import {UserRouter} from "./features/users/user.router";
+import cors from "cors";
+import { UserController } from './features/users/user.controller';
+import {errorsMiddleware} from './middlewares/errorsMiddleware';
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 app.get('/', (req, res) => {
   return res.send('Hola mundo!');
@@ -12,8 +16,12 @@ app.get('/', (req, res) => {
 const apiRouter = Router(); 
 app.use('/api', apiRouter);
 
-const userRouter  = new UserRouter();
-apiRouter.use(userRouter.router) 
+const userController = new UserController();
+
+const userRouter  = new UserRouter(userController);
+apiRouter.use(userRouter.router)
+
+app.use(errorsMiddleware);
 
 if(NODE_ENV != 'production') {
 
